762d373fd6498ea7afc42132a371a17a85d37ce1
Kevin Sendjaja <kevin.sendjaja@gmail.com>
2019-06-10 12:01:31 +0700

    Sidebar highlight switching enabled

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -5,12 +5,30 @@ import './SideBar.scss';
 import logo from '../../assets/images/spartalogo.png';
 
 export class SideBar extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        labels: ['Profile','Dashboard','Submission'],
+        highlights: [null,true,null],
+        icons: ['address card', 'thumb tack', 'file']
+      };
+    }
+
+    handleClick(i) {
+      const labels = this.state.labels.slice();
+      const newHighlight = Array(3).fill(null);
+      const icons = this.state.icons.slice();
+      newHighlight[i] = true;
+      this.setState({labels: labels, highlights: newHighlight, icons: icons});
+    }
+
     render() {
         return (
           <Menu borderless vertical stackable fixed='left' className='side-nav'>
-            <SideBarItem label='Profile' icon='address card'/>
-            <SideBarItem highlight={true} label='Dashboard' icon='thumb tack'/>
-            <SideBarItem label='Submission' icon='file'/>
+            <Menu.Item onClick={() => this.handleClick(0)}><SideBarItem highlight={this.state.highlights[0]} label={this.state.labels[0]} icon={this.state.icons[0]}  /></Menu.Item>
+            <Menu.Item onClick={() => this.handleClick(1)}><SideBarItem highlight={this.state.highlights[1]} label={this.state.labels[1]} icon={this.state.icons[1]}  /></Menu.Item>
+            <Menu.Item onClick={() => this.handleClick(2)}><SideBarItem highlight={this.state.highlights[2]} label={this.state.labels[2]} icon={this.state.icons[2]}  /></Menu.Item>
             <Menu.Menu className='side-nav-container'>
                 <Menu.Item className='logo'>
                     <Image src={logo} size='medium'></Image>
