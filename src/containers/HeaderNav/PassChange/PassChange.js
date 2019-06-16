3981682ff60af9ad4e49320a781b5eca09ee1810
Kevin Sendjaja <kevin.sendjaja@gmail.com>
2019-06-16 18:47:52 +0700

    Add password validation (still buggy)

--- src/containers/HeaderNav/PassChange/PassChange.js
+++ src/containers/HeaderNav/PassChange/PassChange.js
@@ -9,11 +9,41 @@ import {
 } from "semantic-ui-react";
 
 export class PassChange extends React.Component {
-  state = { modalOpen: false };
+  constructor() {
+    super();
+    this.state = {
+      password: "",
+      newPassword: "",
+      reEnter: "",
+      valid: false
+    };
+  }
 
   handleOpen = () => this.setState({ modalOpen: true });
 
   handleClose = () => this.setState({ modalOpen: false });
+
+  handlePassword = evt => {
+    this.setState({ password: evt.target.value });
+    this.checkValid();
+  };
+
+  handleNewPassword = evt => {
+    this.setState({ newPassword: evt.target.value });
+    this.checkValid();
+  };
+
+  handleReEnter = evt => {
+    this.setState({ reEnter: evt.target.value });
+    this.checkValid();
+  };
+
+  checkValid = () => {
+    const { password, newPassword, reEnter } = this.state;
+    newPassword.length >= 8 && newPassword === reEnter
+      ? this.setState({ valid: true })
+      : this.setState({ valid: false });
+  };
 
   render() {
     return (
@@ -37,20 +67,28 @@ export class PassChange extends React.Component {
           <Form>
             <Form.Field>
               <label>Old Password</label>
-              <input type="password" />
+              <input type="password" onChange={this.handlePassword} />
             </Form.Field>
             <Form.Field>
               <label>New Password</label>
-              <input type="password" />
+              <label>Password should be at least 8 characters long</label>
+              <input type="password" onChange={this.handleNewPassword} />
+              <label>{this.state.newPassword}</label>
             </Form.Field>
             <Form.Field>
               <label>Confirm New Password</label>
-              <input type="password" />
+              <input type="password" onChange={this.handleReEnter} />
+              <label>{this.state.reEnter}</label>
+              <label>{this.state.valid}</label>
             </Form.Field>
           </Form>
         </ModalContent>
         <ModalActions>
-          <Button color="green" onClick={this.handleClose}>
+          <Button
+            disabled={!this.state.valid}
+            color="green"
+            onClick={this.handleClose}
+          >
             Submit
           </Button>
         </ModalActions>
