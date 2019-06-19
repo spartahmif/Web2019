44360b545013f3372aca9bddbf8e01fce8f35c70
Yoel Susanto <yoelsusantopro@yahoo.com>
2019-06-19 21:52:47 +0700

    Remove unused code and add semantic ui cdn

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 
 import { authReducer } from "./store/reducers/auth";
-
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import App from "./views/App";
 import "semantic-ui-css/semantic.min.css";
 
 const rootReducer = combineReducers({ auth: authReducer });
@@ -23,8 +21,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
